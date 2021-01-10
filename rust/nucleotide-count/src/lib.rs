23326c30b6eb2786b8ee60b8bf8a433d779b08bc
zhangchunzhong <zczxyz@126.com>
2021-01-11 07:48:28 +0800

    nucleotide-count done, etl stated

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,16 +1,44 @@
 use std::collections::HashMap;
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    unimplemented!(
-        "How much of nucleotide type '{}' is contained inside DNA string '{}'?",
-        nucleotide,
-        dna
-    );
+    let mut ret : usize = 0;
+    let n = vec!['A', 'C', 'G', 'T'];
+    if !n.contains(&nucleotide) {
+        return Err(nucleotide)
+    }
+    for c in dna.chars() {
+        match c {
+            'A' => {
+                if nucleotide == 'A' {ret += 1};
+            },
+            'C' => {
+                if nucleotide == 'C' {ret += 1};
+            }
+            'G' => {
+                if nucleotide == 'G' {ret += 1};
+            },
+            'T' => {
+                if nucleotide == 'T' {ret += 1};
+            },
+            _ => {
+                return Err(c)
+            }
+        }
+    }
+    Ok(ret)
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    unimplemented!(
-        "How much of every nucleotide type is contained inside DNA string '{}'?",
-        dna
-    );
+    let mut ret = HashMap::new();
+    for c in ['A', 'C', 'G', 'T'].iter() {
+        match count(*c, dna) {
+            Ok(count) => {
+                ret.insert(*c, count);
+            }
+            Err(e) => {
+                return Err(e)
+            }
+        }
+    }
+    Ok(ret)
 }
