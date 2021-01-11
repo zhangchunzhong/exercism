a6d58d8fc6a84a6d8aa61ed22da27b49a73a3806
zhangchunzhong <zczxyz@126.com>
2021-01-11 08:21:03 +0800

    etl done, acronym started

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,5 +1,12 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    unimplemented!("How will you transform the tree {:?}?", h)
+    let mut ret = BTreeMap::new();
+    for (k, vev) in h {
+        for  v in vev.iter() {
+            let p = (*v).to_ascii_lowercase();
+            ret.insert(p, *k);
+        }
+    }
+    ret
 }
