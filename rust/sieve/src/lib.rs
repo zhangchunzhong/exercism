de2ddeae90f8bee243479fb30242ef1001b38e71
zhangchunzhong <zczxyz@126.com>
2021-01-11 08:45:29 +0800

    add rna-transcription, done sieve

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,3 +1,20 @@
+pub fn is_prime(n: u64) -> bool {
+    let limit = (n as f64).sqrt() as u64;
+
+    for i in 2..=limit {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    unimplemented!("Construct a vector of all primes up to {}", upper_bound);
+    let mut ret = Vec::new();
+    for n in 2..=upper_bound {
+        if is_prime(n) {
+            ret.push(n);
+        }
+    }
+    ret
 }
