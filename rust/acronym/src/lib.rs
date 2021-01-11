70aade160f8c5083a5431c717545c4deb33034aa
zhangchunzhong <zczxyz@126.com>
2021-01-11 08:38:26 +0800

    add sieve, acronym done

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    let mut sp = ' ';
+    let mut preup:bool = false;
+    let mut ret = String::new();
+    for c in phrase.chars() {
+        if (sp == '-' || sp == ' ' || (c.is_ascii_uppercase() && !preup)) && c.is_ascii_alphabetic() {
+            ret.push(c.to_ascii_uppercase());
+        }
+        sp = c;
+        preup = c.is_ascii_uppercase();
+    }
+    ret
 }
