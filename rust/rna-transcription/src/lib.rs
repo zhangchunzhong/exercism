54331788bbb654773084110f7f2b7a06c65ff5f1
zhangchunzhong <zczxyz@126.com>
2021-01-11 09:06:38 +0800

    add triangle

--- rust/rna-transcription/src/lib.rs
+++ rust/rna-transcription/src/lib.rs
@@ -1,21 +1,52 @@
 #[derive(Debug, PartialEq)]
-pub struct Dna;
+pub struct Dna{
+    s: String,
+}
 
 #[derive(Debug, PartialEq)]
-pub struct Rna;
+pub struct Rna {
+    s: String,
+}
 
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        unimplemented!("Construct new Dna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", dna);
+        let n = vec!['A', 'C', 'T', 'G'];
+        let mut i = 0;
+        for c in dna.chars() {
+            if !n.contains(&c) {
+                return Err(i)
+            }
+            i += 1;
+        }
+        Ok(Dna {s: dna.to_string()})
     }
 
     pub fn into_rna(self) -> Rna {
-        unimplemented!("Transform Dna {:?} into corresponding Rna", self);
+        let mut ret = String::new();
+        for c in self.s.chars() {
+            //G -> C, C -> G, T -> A, A -> U
+            match c {
+                'G' => ret.push('C'),
+                'C' => ret.push('G'),
+                'T' => ret.push('A'),
+                'A' => ret.push('U'),
+                _ => continue,
+            }
+        }
+        Rna {s: ret}
     }
 }
 
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        unimplemented!("Construct new Rna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", rna);
+        let n = vec!['A', 'C', 'G', 'U'];
+        let mut i = 0;
+        for c in rna.chars() {
+            if !n.contains(&c) {
+                return Err(i)
+            }
+            i += 1;
+        }
+        Ok(Rna {s: rna.to_string()})
     }
 }
