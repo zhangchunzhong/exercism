4e4f4c185c7f469f58fab3b3336d6467a29ea8e9
zhangchunzhong <zczxyz@126.com>
2021-01-13 08:42:49 +0800

    update today

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,7 +1,29 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+pub fn optadd(a: Option<usize>, b: Option<usize>) -> Option<usize> {
+    match (a, b) {
+        (Some(a), Some(b)) => Some(a + b),
+        (None, Some(_b)) => None,
+        (Some(a), None) => Some(a),
+        _ => None,
+    }
+}
+
+pub fn find<T, S>(array: S, key: T) -> Option<usize>
+where
+    T: PartialEq + PartialOrd,
+    S: AsRef<[T]>,
+{
+    let array = array.as_ref();
+    let len = array.len();
+    let mid = len / 2;
+    if len >= 1 {
+        if array[mid] == key {
+            Some(mid)
+        } else if array[mid] > key {
+            find(&array[0..mid], key)
+        } else {
+            optadd(find(&array[mid + 1..], key), Some(mid + 1))
+        }
+    } else {
+        None
+    }
 }
