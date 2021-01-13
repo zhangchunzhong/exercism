85bf8e72c8597d37e7f4d0dbcccccb415ee546d0
zhangchunzhong <zczxyz@126.com>
2021-01-13 21:54:51 +0800

    queen attack done, start bowling

--- rust/queen-attack/src/lib.rs
+++ rust/queen-attack/src/lib.rs
@@ -1,31 +1,49 @@
 #[derive(Debug)]
-pub struct ChessPosition;
+pub struct ChessPosition {
+    rank: i32,
+    file: i32,
+}
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen {
+    pos:ChessPosition
+}
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        unimplemented!(
-            "Construct a ChessPosition struct, given the following rank, file: ({}, {}). If the position is invalid return None.",
-            rank,
-            file
-        );
+        if rank >= 8 || file >= 8 {
+            None
+        } else if rank < 0 || file < 0 {
+            None
+        } else {
+            Some(ChessPosition {
+                rank,
+                file,
+            })
+        }
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        unimplemented!(
-            "Given the chess position {:?}, construct a Queen struct.",
-            position
-        );
+        Queen { pos:position }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        unimplemented!(
-            "Determine if this Queen can attack the other Queen {:?}",
-            other
-        );
+        if self.pos.file == other.pos.file {
+            true
+        } else if self.pos.rank == other.pos.rank {
+            true
+        } else {
+            let m = self.pos.rank - self.pos.file;
+            let n = self.pos.rank + self.pos.file;
+            if other.pos.rank == other.pos.file + m {
+                true
+            } else if other.pos.rank + other.pos.file == n {
+                true
+            } else {
+                false
+            }
+        }
     }
 }
