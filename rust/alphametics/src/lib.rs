c5cec62366691b7808e4fba1bda662361dc1f811
zhangchunzhong <zczxyz@126.com>
2021-01-20 22:29:00 +0800

    add alphametics

---
+++ rust/alphametics/src/lib.rs
@@ -0,0 +1,54 @@
+use itertools::Itertools;
+use std::char;
+use std::collections::{HashMap, HashSet};
+
+fn check_equation(puzzle: &str, solution: &HashMap<char, u8>) -> bool {
+    let pstr: String = puzzle
+        .chars()
+        .map(|c| {
+            if let Some(&n) = solution.get(&c) {
+                char::from_digit(n as u32, 10).unwrap()
+            } else {
+                c
+            }
+        })
+        .collect();
+    let equation: Vec<&str> = pstr.split("==").collect();
+    let left: u64 = equation[0]
+        .split('+')
+        .map(str::trim)
+        .map(|x| x.parse::<u64>().unwrap())
+        .sum();
+    let right: u64 = equation[1].trim().parse::<u64>().unwrap();
+    let just_numbers = pstr
+        .chars()
+        .filter(|c| c.is_digit(10) || c.is_whitespace())
+        .collect::<String>();
+    let no_leading_zeroes = just_numbers
+        .split_whitespace()
+        .all(|number| number.chars().next().unwrap() != '0');
+
+    left == right && no_leading_zeroes
+}
+
+pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
+    let letters: HashSet<char> = input
+        .chars()
+        .filter(|&c| c.is_alphabetic() && c.is_uppercase())
+        .collect();
+    let letters: Vec<char> = letters.into_iter().collect();
+    let len = letters.len();
+    let combins = (0..=9).permutations(len);
+    for c in combins {
+        let mut chk = HashMap::new();
+        let mut i = 0;
+        for l in letters.clone() {
+            chk.insert(l, c[i]);
+            i += 1;
+        }
+        if check_equation(input, &chk) {
+            return Some(chk);
+        }
+    }
+    None
+}
